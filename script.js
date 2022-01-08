61a5f77d70f380aad81c1d6c99d8374fefa25e52
Marina <promo_marina@mail.ru>
2022-01-08 19:53:14 +0500

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let num = 5;
+const leftBorder= 15;
+
+num = 5+10;
+console.log(num);
+alert(num);
+
+let con = confirm('Ты здесь?');
+console.log(con);
+alert(con);
+
+let pro = prompt('Вам есть 18?');
+console.log(pro);
