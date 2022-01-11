8cd22bd983aacf04c559d6f190c5b7c8726f3aab
Marina <promo_marina@mail.ru>
2022-01-12 00:12:48 +0500

    12.01.22 пробы

--- пробы.js
+++ пробы.js
@@ -13,3 +13,53 @@ alert(con);
 
 let pro = prompt('Вам есть 18?');
 console.log(pro);
+
+//фукнции
+
+//1
+let a = 5;
+ function addTwo(z) {
+   let ret = z + 2;
+   return ret;
+ }
+ let b = addTwo(a);
+ console.log(b);
+
+
+//2
+let val1 = 2;
+function multiplyThis(n) {
+let ret = n * val1;
+return ret;
+}
+let multiplied = multiplyThis(6);
+console.log('example of scope:', multiplied);
+
+//3
+let val = 7;
+function createAdder() {
+    function addNumbers(a, b) {
+    let ret = a + b;
+    return ret;
+    }
+ return addNumbers;
+}
+let adder = createAdder();
+let sum = adder(val, 8);
+console.log('example of function returning a function: ', sum);
+
+
+//4
+function createCounter() {
+    let counter = 0;
+    const myFunction = function() {
+        counter = counter + 1;
+        return counter;
+        };
+    return myFunction;
+ }
+ const increment = createCounter();
+ const c1 = increment();
+ const c2 = increment();
+ const c3 = increment();
+ console.log('example increment', c1, c2, c3);
